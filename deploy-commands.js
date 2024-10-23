cbb216b188bf8691df7c721d778edb3c82a334e2
tabeaDargatz <92884948+tabeaDargatz@users.noreply.github.com>
2024-10-23 18:42:31 +0000

    added help command, fixed deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -33,7 +33,7 @@ const rest = new REST().setToken(process.env.TOKEN);
 
     // The put method is used to fully refresh all commands in the guild with the current set
     const data = await rest.put(
-      Routes.applicationGuildCommands((process.env.CLIENT_ID, (process.env.GUILD_ID),
+      Routes.applicationGuildCommands(process.env.CLIENT_ID,process.env.GUILD_ID),
       { body: commands },
     );
 
